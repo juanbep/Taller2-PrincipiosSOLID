9fcb6545de591f2eb65e547f4dc1b5ff0da32369
juanbep <56483953+juanbep@users.noreply.github.com>
2021-03-03 10:03:31 -0500

    versión 1.5

---
+++ target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"co.unicauca.parkinglot.access"},{"l":"co.unicauca.parkinglot.domain"},{"l":"co.unicauca.parkinglot.domain.service"},{"l":"co.unicauca.parkinglot.infra"},{"l":"co.unicauca.parkinglot.presentation"}]
